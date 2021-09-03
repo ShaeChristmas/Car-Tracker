29f688a28333f4ab054c13f9185a537420edfc97
ShaeChristmas <shainechristmas@gmail.com>
2021-09-03 12:49:17 +1000

    update index.js file

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//This is the entry Point.
+// Setting up Website Backend.
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware for Webhosting.
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-RequestedWith, Content-Type, Accept");
+    next();
+});
+
+// Redirect
+//app.get();
